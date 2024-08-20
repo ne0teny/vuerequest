2b074d09c77b49575c0b8bf470a6c0a0f04ba6c8
ne0teny <sailaukhanbeknur@gmail.com>
2024-08-20 11:14:15 +0500

    function edit

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Login from '@/views/Login.vue';
+import UserLogin from '@/views/UserLogin.vue'; 
 import RequestsList from '@/views/RequestsList.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/login', name: 'Login', component: Login },
+  { path: '/login', name: 'Login', component: UserLogin }, 
   { path: '/', name: 'RequestsList', component: RequestsList },
   { path: '/request/:id', name: 'EditRequest', component: () => import('@/components/RequestModal.vue') }
 ];
---
+++ src/views/UserLogin.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login-page">
+    <h2>Авторизация</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Логин или Телефон" required />
+      <input type="password" v-model="password" placeholder="Пароль" required />
+      <button type="submit">Войти</button>
+    </form>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: null
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('https://dev.moydomonline.ru/api/auth/login/', {
+          username: this.username,
+          password: this.password
+        });
+
+        localStorage.setItem('authToken', response.data.token); 
+
+        this.$store.commit('SET_AUTH', true);
+        this.$router.push('/');
+      } catch (e) {
+        this.error = 'Неправильные данные';
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.login-page {
+}
+</style>
